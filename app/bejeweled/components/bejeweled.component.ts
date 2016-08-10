d3a625d71e61d9df584629d0707279d58e3c63cf
John Marion <John Marion>
2016-08-09 20:41:09 -0400

    configure tsconfig for c9.io

--- app/bejeweled/components/bejeweled.component.ts
+++ app/bejeweled/components/bejeweled.component.ts
@@ -33,6 +33,54 @@ export class BejeweledComponent implements OnInit {
 
     constructor ( private gemService: GemService ) {}
 
+    private animateCollapse ()
+    {
+        var record = this.board.collapse ();
+        var collapseRecord = record.collapseRecord;
+        var fallRecord = record.fallRecord;
+        var newRecord = record.newRecord;
+
+        console.log ( collapseRecord );
+        // make the gems disappear
+        for ( var i = 0; i < collapseRecord.length; ++i )
+        {
+            createjs.Tween.get ( this.drawBoard
+                [collapseRecord[i][0]] [collapseRecord[i][1]] )
+                .wait ( 400 )
+                .to ( { alpha: 0 }, 100 );
+        }
+
+        // animate the collapse
+        for ( var i = 0; i < fallRecord.length; ++i )
+        {
+            createjs.Tween.get ( this.drawBoard
+                [fallRecord[i].source.y] [fallRecord[i].source.x] )
+                .wait ( 500 )
+                .to ( { y: 50 * ( fallRecord[i].dest.y -
+                    fallRecord[i].source.y ) }, 300 );
+        }
+
+        console.log ( newRecord );
+        for ( var i = 0; i < newRecord.length; ++i )
+        {
+            var gem = this.gemService.getGem (
+                newRecord [i].gemType, newRecord [i].loc.x * 50,
+                newRecord [i].loc.y * 50 );
+            gem.alpha = 0;
+            this.stage.addChild ( gem );
+            createjs.Tween.get ( gem ).wait ( 900 )
+                .to ( { alpha: 1 }, 200 );
+        }
+
+        var self = this;
+
+        // must re initialize everything
+        setTimeout ( function ()
+        {
+            self.resetDrawBoard ();
+            self.state = BejeweledGameState.AWAITING_SELECT;
+        }, 1100 );
+    }
     private eventToBoardLocation ( event: any )
     {
 
@@ -40,6 +88,37 @@ export class BejeweledComponent implements OnInit {
         var y: number = Math.floor ( event.stageY / 50 );
 
         return new BoardLocation ( x, y );
+    }
+    private resetDrawBoard ()
+    {
+
+        this.drawBoard = [];
+        this.stage.removeAllChildren ();
+        var self = this;
+
+        for ( var i = 0; i < 8; ++i )
+        {
+
+            this.drawBoard.push ( [] );
+
+            for ( var j = 0; j < 8; ++j )
+            {
+                var gem = this.gemService.getGem (
+                    this.board.board [i] [j], j * 50, i * 50 );
+                gem.addEventListener ( 'click', function ( event: any )
+                {
+                    self.handleGemClick ( event );
+                });
+                this.drawBoard [i].push ( gem );
+                this.stage.addChild ( gem );
+            }
+        }
+
+        createjs.Ticker.addEventListener ( 'tick', function ()
+        {
+            self.stage.update ();
+        });
+        this.stage.update ();
     }
 
     handleGemClick ( event: any )
@@ -61,8 +140,8 @@ export class BejeweledComponent implements OnInit {
         else if ( this.state == BejeweledGameState.AWAITING_SECOND )
         {
 
+            var self = this;
             this.state = BejeweledGameState.WORKING;
-
             var location = this.eventToBoardLocation ( event );
             if ( this.chosenGemLocation.isAdjacent ( location ) )
             {
@@ -101,44 +180,7 @@ export class BejeweledComponent implements OnInit {
                         DIRECTION_MOVES [ DIRECTION_OPPOSITES [direction] ],
                         300 );
 
-                    var record = this.board.collapse ();
-                    var collapseRecord = record.collapseRecord;
-                    var fallRecord = record.fallRecord;
-                    var newRecord = record.newRecord;
-
-                    console.log ( collapseRecord );
-                    // make the gems disappear
-                    for ( var i = 0; i < collapseRecord.length; ++i )
-                    {
-                        createjs.Tween.get ( this.drawBoard
-                            [collapseRecord[i][0]] [collapseRecord[i][1]] )
-                            .wait ( 400 )
-                            .to ( { alpha: 0 }, 100 );
-                    }
-
-                    // animate the collapse
-                    for ( var i = 0; i < fallRecord.length; ++i )
-                    {
-                        createjs.Tween.get ( this.drawBoard
-                            [fallRecord[i].source.y] [fallRecord[i].source.x] )
-                            .wait ( 500 )
-                            .to ( { y: 50 * ( fallRecord[i].dest.y -
-                                fallRecord[i].source.y ) }, 300 );
-                    }
-
-                    console.log ( newRecord );
-                    for ( var i = 0; i < newRecord.length; ++i )
-                    {
-                        var gem = this.gemService.getGem (
-                            newRecord [i].gemType, newRecord [i].loc.x * 50,
-                            newRecord [i].loc.y * 50 ) );
-                        gem.alpha = 0;
-                        this.stage.addChild ( gem );
-                        createjs.Tween.get ( gem ).wait ( 900 )
-                            .to ( { alpha: 1 } );
-                    }
-
-                    // must re initialize everything
+                    this.animateCollapse ();
                 }
             }
             else
@@ -159,37 +201,12 @@ export class BejeweledComponent implements OnInit {
     }
 
     ngOnInit () {
-
         this.state = BejeweledGameState.WORKING;
         this.board = new BejeweledGameBoard ();
         this.board.initialize ();
         this.stage = new createjs.Stage ( document.getElementById
             ( 'bejeweled-canvas' ) );
-        var self = this;
-
-        for ( var i = 0; i < 8; ++i )
-        {
-
-            this.drawBoard.push ( [] );
-
-            for ( var j = 0; j < 8; ++j )
-            {
-                var gem = this.gemService.getGem (
-                    this.board.board [i] [j], j * 50, i * 50 );
-                gem.addEventListener ( 'click', function ( event: any )
-                {
-                    self.handleGemClick ( event );
-                });
-                this.drawBoard [i].push ( gem );
-                this.stage.addChild ( gem );
-            }
-        }
-
-        createjs.Ticker.addEventListener ( 'tick', function ()
-        {
-            self.stage.update ();
-        });
+        this.resetDrawBoard ();
         this.state = BejeweledGameState.AWAITING_SELECT;
-        this.stage.update ();
     }
 };
